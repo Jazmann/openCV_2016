d1e6900cb8607da94feabccf448cba92383c68d8
Jasper Shemilt <jaspershemilt@virginmedia.com>
2016-01-22 21:41:35 +0000

    .orig added to .gitignore Could be removed from Upstream push. .git ignore Possibly unneeded inclusion in calibinit. Currently commented.

--- modules/calib3d/src/calibinit.cpp
+++ modules/calib3d/src/calibinit.cpp
@@ -60,6 +60,7 @@
 \************************************************************************************/
 
 #include "precomp.hpp"
+// #include "opencv2/imgproc.hpp"
 #include "opencv2/imgproc/imgproc_c.h"
 #include "opencv2/calib3d/calib3d_c.h"
 #include "circlesgrid.hpp"
